1265b5311ac0b53fc148cd0bf2dab61967ec6f22
krasnikovgp <148316952+krasnikovgp@users.noreply.github.com>
2024-01-19 15:44:35 +0300

    Add files via upload

--- webserv.py
+++ webserv.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def hello():
+    return f"""
+    <h1>Мой Сервер</h1>
+    <a href="{url_for('index')}">Сайт</a><br>
+    """
+
+
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/day-<num>')
+def day(num):
+    return render_template(f'day-{num}.html')
+
+
+@app.route('/photo-<num>')
+def photo(num):
+    return render_template(f'photo-{num}.html')
 
 
 if __name__ == '__main__':
